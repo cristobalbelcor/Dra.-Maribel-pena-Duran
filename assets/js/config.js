0369acdd09272562ae717bf4fab07359ed4f9132
cristobal <cristobalibsis@gmail.com>
2026-02-26 00:24:31 -0500

    chore: redirigir botones de agenda a WhatsApp

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
 window.SITE_CONFIG = {
-  ghlCalendar: "https://app.gohighlevel.com/",
+  ghlCalendar: "https://wa.me/573505828278?text=Hola%20Dra.%20Maribel%2C%20quiero%20agendar%20una%20cita.",
   ghlLeadMagnet: "https://app.gohighlevel.com/",
-  ghlDiscoveryCall: "https://app.gohighlevel.com/",
+  ghlDiscoveryCall: "https://wa.me/573505828278?text=Hola%20Dra.%20Maribel%2C%20quiero%20agendar%20una%20llamada%20de%20orientaci%C3%B3n.",
   ghlCheckoutPack: "https://app.gohighlevel.com/",
-  whatsappBusiness: "https://wa.me/573000000000",
+  whatsappBusiness: "https://wa.me/573505828278",
   stripeCheckout: "https://buy.stripe.com/",
   paypalCheckout: "https://www.paypal.com/",
   crmPortal: "https://app.gohighlevel.com/"
